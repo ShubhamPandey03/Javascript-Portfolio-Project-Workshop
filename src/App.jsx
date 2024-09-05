6912321048c2f8ab97b05f9c2c07d2d8b66fac76
Shubham Pandey <64465919+ShubhamPandey03@users.noreply.github.com>
2024-09-05 13:29:09 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Shubham Pandey",
+  title: "Front End Developer",
+  email: "shubhamx@example.com",
+  gitHub: "ShubhamPandey03",
+  instagram: "pandeyshubhamofficial",
+  linkedIn: "shubhampandey003",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "SubhuPndy",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
